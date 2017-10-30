05fdb9b92ea3515debbb2f57bc230697023fe4d6
root <israel_batista.am@hotmail.com>
2017-10-30 08:36:48 -0400

    change files

---
+++ server.js
@@ -0,0 +1,12 @@
+const Screenshot = require('url-to-screenshot')
+const fs = require('fs')
+ 
+new Screenshot('http://g1.globo.com/')
+  .width(800)
+  .height(600)
+  .clip()
+  .capture()
+  .then(img =>{
+    fs.writeFileSync(__dirname + '/example.png', img)
+    console.log('open example.png')
+  })
