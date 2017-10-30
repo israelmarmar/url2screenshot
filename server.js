d832530cae6d1fe3822e3ffb38cce9cb7556c06f
root <israel_batista.am@hotmail.com>
2017-10-30 08:43:55 -0400

    change files

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const Screenshot = require('url-to-screenshot')
 const fs = require('fs')
+const app=require("express")
  
+
+
+app.get('/', function (req, res) {
+
 new Screenshot('http://g1.globo.com/')
   .width(800)
   .height(600)
@@ -10,3 +15,9 @@ new Screenshot('http://g1.globo.com/')
     fs.writeFileSync(__dirname + '/example.png', img)
     console.log('open example.png')
   })
+
+})
+
+app.listen(port, function () {
+ console.log("ligado");
+});
