0ac86aace7035487ce6bd3a5cddb00f9b1fada3d
root <israel_batista.am@hotmail.com>
2017-10-31 10:19:42 -0400

    change files

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
-const Screenshot = require('url-to-screenshot')
 const fs = require('fs')
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;  
 
-app.get('/:url', function (req, res) {
-console.log(req.params.url);
-
-
-new Screenshot(decodeURI(req.params.url))
-  .width(1200)
-  .height(800)
-  .capture()
-  .then(img =>{
-    fs.writeFileSync(__dirname + '/screen.png', img)
-    console.log('open screen.png')
-    res.sendFile(__dirname + '/screen.png');
-  })
-
-})
-
+var webshot = require('webshot');
 
 app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
@@ -29,24 +13,17 @@ app.use(function(req, res, next) {
         next();
     });
 
+app.get('/:url', function (req, res) {
 
-app.get('/cons/https://:url', function (req, res) {
-console.log(req.params.url);
-console.log(req.headers);
-    res.json(req.headers);
+var renderStream = webshot(req.params.url);
+var file = fs.createWriteStream('screen.jpg', {encoding: 'binary'});
+ 
+renderStream.on('data', function(data) {
+  file.write(data.toString('binary'), 'binary');
+  res.sendFile(__dirname+"/screen.jpg");
+});
 
-
-})
-
-
-
-app.get('/cons/http://:url', function (req, res) {
-console.log(req.params.url);
-console.log(req.headers);
-    res.json(req.headers);
-
-
-})
+});
 
 app.listen(port, function () {
  console.log("ligado");
