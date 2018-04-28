0524fff41a8ca51acf67b50edc6c00b7834954c1
Israel Marques Martins <email.israel.br@gmail.com>
2018-04-28 16:51:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const validUrl = require('valid-url');
 
 var parseUrl = function(url) {
