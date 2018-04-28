eef8b5dd5450a787dc3190004a14b05d75545547
israel marques martins <emaill.israel.br@gmail.com>
2018-04-28 15:15:09 -0400

    fix bugs

--- server.js
+++ server.js
@@ -1,75 +1,44 @@
-const fs = require('fs');
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-
+const express = require('express');
+const app = express();
 const puppeteer = require('puppeteer');
+const port = process.env.PORT || 8080;
+const validUrl = require('valid-url');
 
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+var parseUrl = function(url) {
+    url = decodeURIComponent(url)
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
+    }
 
-function encod(string){
-  return Base64.encode(string).replace(/\+|\/|=|:/gi,"");
-}
+    return url;
+};
 
-app.get('/:url', function (req, res) {
-   var resp=res;
+app.get('/', function(req, res) {
+    var urlToScreenshot = parseUrl(req.query.url);
 
-   console.log(req.headers);
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async() => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
 
+            const page = await browser.newPage();
+            await page.goto(urlToScreenshot);
+            await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+                res.setHeader('Content-Type', 'image/png');
+                res.send(buffer)
+            });
 
-
-   const shot = async () => {
-    const file=encod(req.params.url)+".png";
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-
-    const page = await browser.newPage();
-
-    page.setViewport({width:1366,height:768});
-
-    const suc = await page.goto(req.params.url,{timeout:60000});
-    
-
-   console.log(await page.content());
-
-  await page.waitFor(3000);
-
-  const img = await page.screenshot({path: file, fullPage: true});
-
-  if (fs.existsSync(__dirname+"/"+file)) {
-    resp.sendFile(__dirname+"/"+file, function(value){
-        fs.unlinkSync(__dirname+"/"+file);
-    });
-
-}
-
-
-
-await browser.close();
-
-}
-
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
 
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
-
-app.get('/cons/:url', function (req, res) {
-
-    console.log(req.headers);
-    res.json(req.headers);
-
-
+app.listen(port, function() {
+    console.log('App listening on port ' + port)
 })
-
-app.listen(port, function () {
-   console.log("ligado");
-});
