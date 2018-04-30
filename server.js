4c0a0e3d9126d60449c6668560de2bab4974d021
Israel Marques Martins <email.israel.br@gmail.com>
2018-04-30 15:33:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
         console.log('Screenshotting: ' + urlToScreenshot);
         (async() => {
             const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage','--single-process']
             });
 
             const page = await browser.newPage();
