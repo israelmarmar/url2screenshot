3a2f55c614d0c051fcaccbb6761ea1fafb279c91
Israel Marques Martins <email.israel.br@gmail.com>
2018-04-30 10:38:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,45 +5,45 @@ const port = process.env.PORT || 3000;
 const validUrl = require('valid-url');
 
 var parseUrl = function(url) {
-	url = decodeURIComponent(url)
-	if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-		url = 'http://' + url;
-	}
+    url = decodeURIComponent(url)
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = 'http://' + url;
+    }
 
-	return url;
+    return url;
 };
 
 app.get('/', function(req, res) {
-	var urlToScreenshot = parseUrl(req.query.url);
+    var urlToScreenshot = parseUrl(req.query.url);
 
-	if (validUrl.isWebUri(urlToScreenshot)) {
-		console.log('Screenshotting: ' + urlToScreenshot);
+    if (validUrl.isWebUri(urlToScreenshot)) {
+        console.log('Screenshotting: ' + urlToScreenshot);
+        (async() => {
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
 
-		puppeteer.launch().then(async browser => {
-			const page = await browser.newPage();
+            const page = await browser.newPage();
 
-		await page.goto(urlToScreenshot,{timeout:60000});
+            await page.goto(urlToScreenshot,{timeout:60000});
 
-		await page.waitFor(3000);
+            await page.waitFor(3000);
 
+            
+            await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+                res.setHeader('Content-Type', 'image/png');
+                res.send(buffer);
+            });
 
-		await page.screenshot({fullPage: true}).then(function(buffer) {
-			res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-			res.setHeader('Content-Type', 'image/png');
-			res.send(buffer);
-		});
-
-		await browser.close();
-  // ...
-		});
-
-
-	} else {
-		res.send('Invalid url: ' + urlToScreenshot);
-	}
+            await browser.close();
+        })();
+    } else {
+        res.send('Invalid url: ' + urlToScreenshot);
+    }
 
 });
 
 app.listen(port, function() {
-	console.log('App listening on port ' + port)
+    console.log('App listening on port ' + port)
 })
