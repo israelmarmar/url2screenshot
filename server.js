5d511587b16401f028999259264acd978185647c
Israel Marques Martins <email.israel.br@gmail.com>
2018-05-07 08:09:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
 
             const page = await browser.newPage();
             
-            await page.setViewport({ width: 1024, height: 768 });
+            await page.setViewport({ width: 1440, height: 900 });
 
             await page.goto(urlToScreenshot);
 
